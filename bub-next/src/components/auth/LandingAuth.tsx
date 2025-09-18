251695ee8c12dc5e0682b61be07a4f83d64088b0
abrartanim <abrar.tanim@gmail.com>
2025-09-18 22:36:17 +0600

    added authentication page

---
+++ bub-next/src/components/auth/LandingAuth.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { FaGoogle } from "react-icons/fa";
+export default function LandingAuth() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center p-4">
+      <div className="">
+        <h1 className="text-4xl font-bold text-white">Welcome Back</h1>
+        <h3 className="mt-2   text-gray-400 ">
+          Login or Signup with google to continue with Barely Used Bytes
+        </h3>
+      </div>
+
+      <div className="mt-4">
+        <button
+          type="submit"
+          className="flex items-center gap-3 px-6 py-3 font-semibold bg-[#2c4c46] rounded-lg hover:bg-[#3a615a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-[#1a2e2a] focus:ring-[#66f9d1] cursor-pointer"
+        >
+          <FaGoogle size={20} />
+          Click me to move forward :)
+        </button>
+      </div>
+    </div>
+  );
+}
